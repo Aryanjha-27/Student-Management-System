2ccbb102eefa387a8880be0a303f0e42fa64a34a
Aryanjha-27 <jhaaryan615@gmail.com>
2025-08-07 19:56:45 +0545

    bootstrap added

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="Style/css/bootstrap.min.css">
+</head>
+<body>
+    <h1>Hello</h1>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"></script>
+    
+</body>
+</html>
